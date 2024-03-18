98c6213f140b8c5ca845cafbdc49384de0f18214
Alisson Youssf <youssfbr@gmail.com>
2024-03-18 20:10:46 -0300

    Backend Project created

---
+++ src/test/java/com/github/youssfbr/awpag/AwpagApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.github.youssfbr.awpag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AwpagApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
