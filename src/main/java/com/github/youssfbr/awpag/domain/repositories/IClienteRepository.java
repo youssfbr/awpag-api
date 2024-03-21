ce7e988f49ef0abbcfdcfe9c318344811a105c4b
Alisson Youssf <youssfbr@gmail.com>
2024-03-20 21:32:25 -0300

    Cliente findByNome

--- src/main/java/com/github/youssfbr/awpag/domain/repositories/IClienteRepository.java
+++ src/main/java/com/github/youssfbr/awpag/domain/repositories/IClienteRepository.java
@@ -3,5 +3,9 @@ package com.github.youssfbr.awpag.domain.repositories;
 import com.github.youssfbr.awpag.domain.models.Cliente;
 import org.springframework.data.jpa.repository.JpaRepository;
 
+import java.util.List;
+
 public interface IClienteRepository extends JpaRepository<Cliente, Long> {
+    List<Cliente> findByNome(String nome);
+    List<Cliente> findByNomeContaining(String nome);
 }
