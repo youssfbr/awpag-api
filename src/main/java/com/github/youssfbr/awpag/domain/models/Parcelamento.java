282ec7a0d6b0ef80828dfd66dba63e5cf3bfe56c
Alisson Youssf <youssfbr@gmail.com>
2024-03-23 19:20:53 -0300

    Parcelamento entity ORM

---
+++ src/main/java/com/github/youssfbr/awpag/domain/models/Parcelamento.java
@@ -0,0 +1,38 @@
+package com.github.youssfbr.awpag.domain.models;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.math.BigDecimal;
+import java.time.LocalDateTime;
+
+@Entity
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+@Table(name = "tb_parcelamentos")
+@EqualsAndHashCode(onlyExplicitlyIncluded = true)
+public class Parcelamento {
+
+    @Id
+    @EqualsAndHashCode.Include
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @ManyToOne
+    @Column(nullable = false)
+    private Cliente cliente;
+
+    @Column(length = 60 , nullable = false)
+    private String descricao;
+
+    @Column(nullable = false)
+    private BigDecimal valorTotal;
+
+    @Column(nullable = false)
+    private Integer quantidadeParcelas;
+
+    @Column(nullable = false)
+    private LocalDateTime dataCriacao;
+}
